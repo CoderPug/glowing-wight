ea0cfcaff4342b655ff5388b29498c7d181c1b30
coch3 <torrescardenasjose@gmail.com>
2013-09-08 05:20:46 -0500

    Added base project with landing page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomy::Application.config.secret_token = '1fc3b6a43795b371f12033802fb1bb3044d317f7bd7147238337471e3c95a425be18d91207228d9350f9549bd76148a8c18779b31103d6c58c0fdc5b75ab2f9f'
